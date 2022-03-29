85ebbb4df28cc9f38d05e595cf243c9586a72c23
Andrey Marchenko <andrey.marchenko@shopify.com>
2022-03-29 12:35:59 +0200

    where queries

--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -38,4 +38,13 @@ class ActiveRecordTest < Minitest::Test
     assert_kind_of Hash, row
     assert_equal [:id, :title, :body, :created_at, :updated_at], row.keys
   end
+
+  def test_where
+    relation = Post.where("id = 1001").where("title IS NOT NULL")
+    assert_equal "SELECT * FROM posts WHERE id = 1001 AND title IS NOT NULL", relation.to_sql
+    post = relation.first
+    assert_kind_of Post, post
+    assert_equal 1001, post.id
+    assert_equal "My first post", post.title
+  end
 end
