4068477f5e0df91c7e8132448ed7a282fd505bf4
Andrey Marchenko <andrey.marchenko@shopify.com>
2022-03-29 12:23:28 +0200

    refactor active_record requires

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -1,44 +1,4 @@
-require "active_record/connection_adapter"
-
 module ActiveRecord
-  class Base
-    def self.primary_abstract_class
-    end
-
-    def initialize(attributes = {})
-      @attributes = attributes
-    end
-
-    # the real active record dynamically generates methods for each column
-    def method_missing(method, *args)
-      columns = self.class.connection.columns(self.class.table_name)
-
-      if columns.include?(method)
-        @attributes[method]
-      else
-        super
-      end
-    end
-
-    def self.find(id)
-      attributes = connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_s}").first
-      new(attributes)
-    end
-
-    def self.all
-      connection.execute("SELECT * FROM #{table_name}").map { |attributes| new(attributes) }
-    end
-
-    def self.establish_connection(options)
-      @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
-    end
-
-    def self.connection
-      @@connection
-    end
-
-    def self.table_name
-      name.downcase + "s"
-    end
-  end
+  autoload :Base, "active_record/base"
+  autoload :ConnectionAdapter, "active_record/connection_adapter"
 end
---
+++ lib/active_record/base.rb
@@ -0,0 +1,45 @@
+module ActiveRecord
+  class Base
+    def self.primary_abstract_class
+    end
+
+    def initialize(attributes = {})
+      @attributes = attributes
+    end
+
+    # the real active record dynamically generates methods for each column
+    def method_missing(method, *args)
+      columns = self.class.connection.columns(self.class.table_name)
+
+      if columns.include?(method)
+        @attributes[method]
+      else
+        super
+      end
+    end
+
+    def self.find(id)
+      find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_s}").first
+    end
+
+    def self.all
+      find_by_sql("SELECT * FROM #{table_name}")
+    end
+
+    def self.find_by_sql(sql)
+      connection.execute(sql).map { |attributes| new(attributes) }
+    end
+
+    def self.establish_connection(options)
+      @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
+    end
+
+    def self.connection
+      @@connection
+    end
+
+    def self.table_name
+      name.downcase + "s"
+    end
+  end
+end
